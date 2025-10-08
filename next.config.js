571a4d4bb7e1e68a8c723b1b8255216a79f2bb9b
pepars-Rashid <peparsrashid@gmail.com>
2025-10-08 13:57:15 +0300

    url config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+/* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "files.edgestore.dev",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
